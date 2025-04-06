d4c3109b139498b411579761292128507eea0eb9
Dakudemon23 <dakudemon23@gmail.com>
2025-04-06 15:08:57 -0500

    Taller2 vol.1

--- main.ts
+++ main.ts
@@ -1,20 +1,44 @@
 import { series } from "./data.js";
 
 const tableBody: HTMLElement = document.getElementById("series-table-body")!;
+const detailContainer: HTMLElement = document.getElementById("series-detail")!;
+const averageContainer: HTMLElement = document.getElementById("seasons-average")!;
 let totalSeasons: number = 0;
 
 series.forEach((serie, index) => {
   const row = document.createElement("tr");
   row.innerHTML = `
     <th scope="row">${index + 1}</th>
-    <td>${serie.title}</td>
+    <td class="serie-title text-primary" style="cursor:pointer">${serie.title}</td>
     <td>${serie.network}</td>
     <td>${serie.seasons}</td>
   `;
+
+  // boton serie
+  row.querySelector(".serie-title")?.addEventListener("click", () => {
+    renderSerieDetail(serie);
+  });
+
   tableBody.appendChild(row);
   totalSeasons += serie.seasons;
 });
 
+//  promedio
 const average = totalSeasons / series.length;
-const averageContainer = document.getElementById("seasons-average")!;
 averageContainer.textContent = `Seasons average: ${average.toFixed(0)}`;
+
+// Cad
+function renderSerieDetail(serie: any): void {
+  detailContainer.innerHTML = `
+    <div class="card" style="width: 30rem;">
+      <img src="${serie.imageUrl}" class="card-img-top" alt="${serie.title}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.title}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.url}" target="_blank" class="btn btn-primary">Go to series</a>
+      </div>
+    </div>
+  `;
+}
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,37 @@
 import { series } from "./data.js";
-
 const tableBody = document.getElementById("series-table-body");
+const detailContainer = document.getElementById("series-detail");
+const averageContainer = document.getElementById("seasons-average");
 let totalSeasons = 0;
-
 series.forEach((serie, index) => {
+    var _a;
     const row = document.createElement("tr");
     row.innerHTML = `
     <th scope="row">${index + 1}</th>
-    <td>${serie.title}</td>
+    <td class="serie-title text-primary" style="cursor:pointer">${serie.title}</td>
     <td>${serie.network}</td>
     <td>${serie.seasons}</td>
   `;
+    // boton serie
+    (_a = row.querySelector(".serie-title")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+        renderSerieDetail(serie);
+    });
     tableBody.appendChild(row);
     totalSeasons += serie.seasons;
 });
-
+//  promedio
 const average = totalSeasons / series.length;
-const averageContainer = document.getElementById("seasons-average");
 averageContainer.textContent = `Seasons average: ${average.toFixed(0)}`;
+// Cad
+function renderSerieDetail(serie) {
+    detailContainer.innerHTML = `
+    <div class="card" style="width: 30rem;">
+      <img src="${serie.imageUrl}" class="card-img-top" alt="${serie.title}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.title}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.url}" target="_blank" class="btn btn-primary">Go to series</a>
+      </div>
+    </div>
+  `;
+}
